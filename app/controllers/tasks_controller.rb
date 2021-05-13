4b5006d69195692b4ee9c5ac5ef5d9f889faeb32
Mr-Kyary <mr_kyary@icloud.com>
2021-05-14 00:37:17 +0900

    多少の変更。フォーム周辺の表記を変更。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     def index
-        @tasks  = Task.order('limit_date').all
+        @tasks = Task.order('limit_date').all
         @status = ['todo', 'doing', 'done']  
     end
 
@@ -10,30 +10,30 @@ class TasksController < ApplicationController
         task.state = params[:state]
         task.limit_date = params[:limit_date]
         task.save
-        redirect_to '/tasks', notice: 'Task is successfully created!。'
+        redirect_to '/tasks', notice: 'Task is successfully created!'
     end
 
     def show
-        id      = params[:id]
-        @task   = Task.find(id)
+        id = params[:id]
+        @task = Task.find(id)
         @status = ['todo', 'doing', 'done']
     end
 
     def update
         id   = params[:id]
         task = Task.find(1)
-    
-        task.task         = params[:task]
-        task.state        = params[:state]
-        task.limit_date   = params[:limit_date]
+
+        task.task = params[:task]
+        task.state = params[:state]
+        task.limit_date = params[:limit_date]
         task.save
     
         redirect_to '/tasks', notice: 'Task is updated!'
     end
 
     def destroy
-        task       = Task.find(params[:id])
+        task = Task.find(params[:id])
         task.destroy
-        redirect_to '/tasks', notice: 'タスクを削除しました。'
+        redirect_to '/tasks', notice: 'Task is deleted...'
     end
 end
