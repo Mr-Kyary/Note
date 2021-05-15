e2b891739866f3ac7366bab0c32b47dbd996dc88
Mr-Kyary <mr_kyary@icloud.com>
2021-05-15 22:22:43 +0900

    route of root matches to task#index is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get    'tasks', to: 'tasks#index'
-  post   'tasks', to: 'tasks#store'
-  get    'tasks/:id', to: 'tasks#show'
-  put    'tasks/:id', to: 'tasks#update'
+  get '/',  to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#store'
+  get 'tasks/:id', to: 'tasks#show'
+  put 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
 end
